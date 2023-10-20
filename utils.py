c10c8d90c812b9da3e86f1782ec6f21a3cfafe5e
m22aie250@iitj.ac.in <root@Mustafa>
2023-10-21 02:37:41 +0530

    Untrack deleted model files

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn import svm, datasets, metrics
+from sklearn import svm, datasets, metrics, tree
 from sklearn.tree import DecisionTreeClassifier
 
 from joblib import dump, load
@@ -19,25 +19,25 @@ def get_hyperparameter_combinations(dict_of_param_lists):
         base_combinations = get_combinations(param_name, param_values, base_combinations)
     return base_combinations
 
-def tune_hparams(X_train, y_train, X_dev, y_dev, h_params_combinations):
+def tune_hparams(X_train, y_train, X_dev, y_dev, h_params_combinations, model_type="svm"):
     best_accuracy = -1
     best_model_path = ""
     for h_params in h_params_combinations:
         # 5. Model training
         #model = train_model(X_train, y_train, h_params, model_type="svm")
-        model = train_model(X_train, y_train, h_params, model_type="decision_tree")
+        model = train_model(X_train, y_train, h_params, model_type=model_type)
         # Predict the value of the digit on the test subset        
         cur_accuracy = predict_and_eval(model, X_dev, y_dev)
         if cur_accuracy > best_accuracy:
             best_accuracy = cur_accuracy
             best_hparams = h_params
-            best_model_path = "./models/best_model" +"_".join(["{}:{}".format(k,v) for k,v in h_params.items()]) + ".joblib"
+            best_model_path = "./models/best_model " +"_".join(["{}:{}".format(k,v) for k,v in h_params.items()]) + ".joblib"
             best_model = model
 
     # save the best_model    
     dump(best_model, best_model_path) 
 
-    print("Model save at {}".format(best_model_path))
+    # print("Model save at {}".format(best_model_path))
 
     return best_hparams, best_model_path, best_accuracy 
 
@@ -56,21 +56,23 @@ def preprocess_data(data):
 # Split data into 50% train and 50% test subsets
 def split_data(x, y, test_size, random_state=1):
     X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=test_size,random_state=random_state
+    x, y, test_size=test_size, shuffle = True
     )
     return X_train, X_test, y_train, y_test
 
 
 def train_model(x, y, model_params, model_type="svm"):
-    if model_type == "svm":
-        clf = svm.SVC(**model_params)  # For SVM
-    elif model_type == "decision_tree":
-        clf = DecisionTreeClassifier()  # For Decision Tree
-    else:
-        raise ValueError("Invalid model_type. Supported values are 'svm' and 'decision_tree'.")
+    if model_type == "svm":                
+        clf = svm.SVC  # For SVM
+    if model_type == "tree":
+        clf = tree.DecisionTreeClassifier  # For Decision Tree
+    # else:
+    #     raise ValueError("Invalid model_type. Supported values are 'svm' and 'decision_tree'.")
 
     # Train the model
-    model = clf.fit(x, y)
+    model = clf(**model_params)
+    model.fit(x,y)
+    # model = clf.fit(x, y)
     return model
 
 
