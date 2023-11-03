52217bfcb582db4619c6dafffc9154eee29b4df1
m22aie250@iitj.ac.in <root@Mustafa>
2023-11-03 05:32:44 +0530

    Assignment4: volume mounted inside container

---
+++ Docker/docker_run.sh
@@ -0,0 +1,19 @@
+# From  ubuntu:22.04
+FROM python:3.11
+
+COPY . /digits/
+# 
+# RUN apt-get update
+# RUN apt-get install -y python3
+RUN pip3 install -r ./digits/requirements.txt
+
+WORKDIR /digits
+
+VOLUME /digits/models
+
+# CMD python exp.py 3
+
+CMD ["pytest"]
+# CMD ["echo", "1st cmd line"]
+
+
